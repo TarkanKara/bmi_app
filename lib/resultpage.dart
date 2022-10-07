39a443297c623b9a652c4717f6296b2afcd5c190
Burak ŞEKER <xburakseker@gmail.com>
2022-10-07 12:57:54 +0300

    HomePage finished

---
+++ lib/resultpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ResultPage extends StatefulWidget {
+  const ResultPage({super.key, required double result});
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
